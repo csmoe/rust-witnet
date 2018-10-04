2ec10f08e7032bac2edd8b9fe4eb48a31627d44f
csmoe <35686186+csmoe@users.noreply.github.com>
2018-10-04 17:15:04 +0800

    build: generate protocol file

--- data_structures/src/lib.rs
+++ data_structures/src/lib.rs
@@ -1,7 +1,8 @@
 //! data structures
 
 // To enable `#[allow(clippy::all)]`
+#![allow(non_camel_case_types,  non_snake_case,  non_upper_case_globals)]
 #![feature(tool_lints)]
 
 /// Generated Message module from Flatbuffers compiler
-pub mod flatbuffers;
+include!(concat!(env!("OUT_DIR"),  "/protocol_generated.rs"));
